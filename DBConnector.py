18a21f42eb27bdd1bea9cd0f46754558bfe57148
hdelas <hdelas@hotmail.com>
2022-10-26 14:15:50 +0200

    VersionDataBase

--- DBConnector.py
+++ DBConnector.py
@@ -1,30 +1,52 @@
 import sqlite3
 
-def conectar():
+def connect_db():
     db = sqlite3.connect("DataLakeDB.db")
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, User varchar(250) NOT NULL UNIQUE, Contraseña varchar(250) NOT NULL)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS ControlDataLake (ID INTEGER(5) NOT NULL UNIQUE,Table varchar(250) ,TableID	INTEGER(5) NOT NULL UNIQUE,UploadDate	varchar(250)  NOT NULL,ModificationDate	varchar(250))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS Users (id INTEGER(30) PRIMARY KEY, User varchar(250) NOT NULL UNIQUE, Contraseña varchar(250) NOT NULL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS ControlDataLake (TableID varchar(50) NOT NULL,TableName varchar(250) ,UploadDate varchar(250)  NOT NULL, ModificationDate varchar(250),Origin varchar(250),Destiny varchar(250) )")
+    #
 
-    #UploadDate #Table #Size #Modification Date
-    #cursor.execute("INSERT INTO books VALUE(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+    #,Origin varchar(250),Destiny varchar(250)
     db.commit()
     return db
 
 
-def guardar_bd(User):
+def Save_User(User):
     
-     db = conectar()    
+     db = connect_db()    
      user = User.usuario
      contra = User.contraseña
 
      cursor = db.cursor()
-     cursor.execute("INSERT INTO Usuarios(usuario,Contraseña) VALUES(?, ?)",(user, contra))
+     cursor.execute("INSERT INTO ControlDataLake(TableID,UploadDate) VALUES(?, ?)",(user, contra))
      
      db.commit()
      return print("Todo ok")
+
+ #We create a function that will be use for register table operations
+def Update (TableID,Table_Name,UploadDate,ModificationDate,Origin,Destiny): 
+    TableIDstr = str(TableID)
+    db = connect_db()    
+    print(TableID,Table_Name,UploadDate,ModificationDate,Origin,Destiny)
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO ControlDataLake(TableID,TableName,UploadDate,ModificationDate,Origin,Destiny) VALUES(?,?,?,?,?,?)",(TableIDstr,Table_Name,UploadDate,ModificationDate,Origin,Destiny))
+    db.commit()
+
+ #We create a function that will be use to get info from Data Governede table
+def readControlDataLake (TableID):
+    db = sqlite3.connect("Usuarios.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM ControlDataLake where TableID = ? ",[TableID])
+    respuesta_consulta = cursor.fetchall()
+    if len(respuesta_consulta) >0:
+        print(respuesta_consulta)
+        lista = respuesta_consulta[0]
+    else:
+        lista=""
+    return lista
  
- 
+
 def consultar_bd(usuario):
     db = sqlite3.connect("Usuarios.db")
     cursor = db.cursor()
@@ -38,4 +60,5 @@ def consultar_bd(usuario):
     #contra = User.contraseña
     return lista
 
-conectar ()
+
+     
